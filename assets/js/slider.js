2318724dadf3f1ad3adddf01df8563ede4c89925
David Bucheli <davincho1406@gmail.com>
2022-04-10 00:18:29 -0500

    Se organizaron los archivos html css y css

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,6 +1,6 @@
 const slider = document.querySelector("#slider");
-var sliderSection = document.querySelectorAll(".slider__section");
-var sliderSectionLast =  sliderSection[sliderSection.length -1];
+let sliderSection = document.querySelectorAll(".slider__section");
+let sliderSectionLast =  sliderSection[sliderSection.length -1];
 
 const btnLeft = document.querySelector("#btn-left");
 const btnRight = document.querySelector("#btn-right");
@@ -8,7 +8,7 @@ const btnRight = document.querySelector("#btn-right");
 slider.insertAdjacentElement('afterbegin', sliderSectionLast);
 
 function Next() {
-    var sliderSectionFirst = document.querySelectorAll(".slider__section")[0];
+    let sliderSectionFirst = document.querySelectorAll(".slider__section")[0];
     slider.style.marginLeft = "-200%";
     slider.style.transition = "all 0.5s";
     setTimeout(function(){
@@ -19,8 +19,8 @@ function Next() {
 }
 
 function Prev() {
-    var sliderSection = document.querySelectorAll(".slider__section");
-    var sliderSectionLast = sliderSection[sliderSection.length -1];
+    let sliderSection = document.querySelectorAll(".slider__section");
+    let sliderSectionLast = sliderSection[sliderSection.length -1];
     slider.style.marginLeft = "0";
     slider.style.transition = "all 0.5s";
     setTimeout(function(){
